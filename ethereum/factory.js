407937fd3ea8283c6348fd224dda2675d82db212
davidmgudeman <davidmgudeman@gmail.com>
2018-01-17 12:11:32 -0800

    working on the show. Got the specific address from the url with a props.query call then made a new instance of this specific contract by making a new Campaign. Then got a summary on this campaign instance with getSummary method on the new campaign.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x70c4882eA99da417AbB9dAfa512FA545450870d6',
+  '0x27aB21f36e64Cc24CD27384ca0648A40F37800F6',
 );
 
 export default instance;
