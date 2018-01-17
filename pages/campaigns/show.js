555c14a3d9efa95567455621448b843a25a562bf
davidmgudeman <davidmgudeman@gmail.com>
2018-01-17 12:34:13 -0800

    made a card for show.js that shows the manager property

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 
@@ -6,13 +7,47 @@ class CampaignShow extends Component {
   static async getInitialProps(props) {
     const campaign = await Campaign(props.query.address);
     const summary = await campaign.methods.getSummary().call();
-    console.log(summary);
-    return {};
+    return {
+      address: props.query.address,
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
   }
+
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount,
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Manager',
+        description:
+          'The manager created this campaign and can create requests',
+        style: { overflowWrap: 'break-word' },
+      },
+      // {
+      //   header: ,
+      //   meta: ,
+      //   description: ,
+      // },
+    ];
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
         <h1>Hi there CampaignShow</h1>
+        {this.renderCards()}
       </Layout>
     );
   }
