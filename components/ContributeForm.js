15bb202ff34b91f79058b857f5ace9709117e657
davidmgudeman <davidmgudeman@gmail.com>
2018-01-17 15:38:22 -0800

    ContributeForm is complete

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -7,12 +7,15 @@ import { Router } from '../routes';
 class ContributeForm extends Component {
   state = {
     value: '',
+    errorMessage: '',
+    loading: false,
   };
 
   onSubmit = async event => {
     event.preventDefault();
 
     const campaign = Campaign(this.props.address);
+    this.setState({ loading: true, errorMessage: '' });
 
     try {
       await console.log('it fired2');
@@ -23,11 +26,14 @@ class ContributeForm extends Component {
         value: web3.utils.toWei(this.state.value, 'ether'),
       });
       Router.replaceRoute(`/campaigns/${this.props.address}`);
-    } catch (err) {}
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ loading: false, value: '' });
   };
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -37,7 +43,10 @@ class ContributeForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>Contribute!</Button>
+        <Message error header="Ooops" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute!
+        </Button>
       </Form>
     );
   }
