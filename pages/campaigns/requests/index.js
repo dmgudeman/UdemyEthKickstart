75ad954fac0f917160cfc0b2be10ae78ab722ee9
davidmgudeman <davidmgudeman@gmail.com>
2018-01-18 10:46:37 -0800

    header for table for RequestIndex

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
@@ -17,10 +17,10 @@ class RequestIndex extends Component {
           return campaign.methods.requests(index).call();
         }),
     );
-    console.log(requests);
-    return { address };
+    return { address, requests, requestCount };
   }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
     return (
       <Layout>
         <h3>Request List</h3>
@@ -29,6 +29,19 @@ class RequestIndex extends Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     );
   }
